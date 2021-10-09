ea217e97eb617c81da4e6352baa0eb9d52eb37d1
Gu Qi <ogtc@hotmail.com>
2021-10-09 18:13:42 +0800

    构建裸机执行环境

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cargo build --target riscv64gc-unknown-none-elf
-qemu-riscv64 target/riscv64gc-unknown-none-elf/debug/os; echo $?
+RUST_TARGET=riscv64gc-unknown-none-elf
+RUSTSBI_QEMU=../../rcore-os/rustsbi-qemu/target/riscv64imac-unknown-none-elf/release/rustsbi-qemu.bin
+
+cargo build --target $RUST_TARGET --release
+rust-objcopy --binary-architecture=riscv64 target/$RUST_TARGET/release/os --strip-all -O binary target/$RUST_TARGET/release/os.bin
+qemu-system-riscv64 -machine virt -nographic -bios $RUSTSBI_QEMU -device loader,file=target/$RUST_TARGET/release/os.bin,addr=0x80200000
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,35 @@
 #![no_std]
 #![no_main]
 #![feature(llvm_asm)]
+#![feature(global_asm)]
+#![feature(panic_info_message)]
 
-mod lang_items;
+mod sbi;
 
+global_asm!(include_str!("entry.asm"));
 
-const SYSCALL_EXIT: usize = 93;
+use sbi::sbi_call;
 
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+const SBI_SHUTDOWN: usize = 8;
 
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
-    unsafe {
-        llvm_asm!("ecall"
-            : "={x10}" (ret)
-            : "{x10}" (args[0]), "{x11}" (args[1]), "{x12}" (args[2]), "{x17}" (id)
-            : "memory"
-            : "volatile"
-        );
-    }
-    ret
+pub fn console_putchar(c: usize) {
+    sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
 }
 
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
-}
-
-const SYSCALL_WRITE: usize = 64;
-
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-  syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+pub fn shutdown() -> ! {
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    panic!("It should shutdown!");
 }
 
 use core::fmt::{self, Write};
 
 struct Stdout;
-
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        sys_write(1, s.as_bytes());
+        for c in s.chars() {
+            console_putchar(c as usize);
+        }
         Ok(())
     }
 }
@@ -61,7 +53,29 @@ macro_rules! println {
 }
 
 #[no_mangle]
-extern "C" fn _start() {
+extern "C" fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
+#[no_mangle]
+extern "C" fn rust_main() {
     println!("Hello, World!");
-    sys_exit(9);
+    panic!("It should shutdown!");
+}
+
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!("Panicked at {}:{} {}", location.file(), location.line(), info.message().unwrap());
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
 }
